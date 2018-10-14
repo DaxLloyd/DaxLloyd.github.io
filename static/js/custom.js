9f20370c4065017e94f71ef45ed89280caf2fe90
DaxCodeDojo <DaxLloyd28@gmail.com>
2018-10-14 15:35:09 -0400

    Fixed Mobile Navbar, updated content.

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,9 +1,13 @@
 var vh = window.innerHeight;
 
 $(document).ready(function(){
-  $(".hidden").delay(500).fadeIn(2500);
+  
+  $(".fade-in").delay(500).fadeIn(2500);
+  
   $(window).scroll(function () {
-    if ($(this).scrollTop() >= vh ) { 
+    var windowHeight = $(this).scrollTop();
+    
+    if (windowHeight >= vh ) { 
       $("#navbar").css("background", "#212121");
       $(".nav-title").addClass("nav-title-dark-bg");
       $(".nav-link").addClass("nav-link-dark-bg");
@@ -12,5 +16,5 @@ $(document).ready(function(){
       $(".nav-title").removeClass("nav-title-dark-bg");
       $(".nav-link").removeClass("nav-link-dark-bg");
     }
-  });
+  });        
 });
